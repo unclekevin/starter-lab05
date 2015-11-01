44762bae2a2fd88696582a1cd567650e05a60fb2
unclekevin <kevintangeman@dccnet.com>
2015-10-31 23:15:29 -0700

    Step 2 completed. Signed-off-by:unclekevin <kevintangeman@dccnet.com>

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -22,6 +22,11 @@ class Welcome extends Application {
     {
 	$this->data['pagebody'] = 'justone';    // this is the view we want shown
 	$this->data = array_merge($this->data, (array) $this->quotes->last());
+        $this->caboose->needed('jrating','hollywood');
+        
+        $this->data['average'] = ($this->data['vote_count'] > 0) ?
+            ($this->data['vote_total'] / $this->data['vote_count']) : 0; 
+        
 	$this->render();
     }
 
